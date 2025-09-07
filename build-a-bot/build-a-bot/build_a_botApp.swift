3d6f40086944fa5ce704be0706e4bcdf11e3d3a6
Nikhil Sathyanarayana <nikhil.sath99@gmail.com>
2025-09-07 19:08:55 +0100

    package install

--- build-a-bot/build-a-bot/build_a_botApp.swift
+++ build-a-bot/build-a-bot/build_a_botApp.swift
@@ -1,38 +1,44 @@
-//
-//  build_a_botApp.swift
-//  build-a-bot
-//
-//  Created by Nikhil Sathyanarayana on 07/09/2025.
-//
 import SwiftUI
-import SwiftData
+
 import FirebaseCore
 
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
+
 @main
-struct build_a_botApp: App {
-    // SwiftData container
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+struct YourApp: App {
 
-    init() {
-        // Firebase init
-        FirebaseApp.configure()
+  // register app delegate for Firebase setup
+
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+
+    WindowGroup {
+
+      NavigationView {
+
+        ContentView()
+
+      }
+
     }
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
-    }
+  }
+
 }
