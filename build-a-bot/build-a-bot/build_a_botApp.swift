e755c855233c18e15da3c4451b26102961c8e63c
Nikhil Sathyanarayana <nikhil.sath99@gmail.com>
2025-09-07 19:59:54 +0100

    Provide SwiftData modelContainer and init Firebase

--- build-a-bot/build-a-bot/build_a_botApp.swift
+++ build-a-bot/build-a-bot/build_a_botApp.swift
@@ -1,44 +1,18 @@
 import SwiftUI
-
+import SwiftData
 import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-  func application(_ application: UIApplication,
-
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
-    FirebaseApp.configure()
-
-    return true
-
-  }
-
-}
-
-
 @main
-
-struct YourApp: App {
-
-  // register app delegate for Firebase setup
-
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-
-  var body: some Scene {
-
-    WindowGroup {
-
-      NavigationView {
-
-        ContentView()
-
-      }
-
+struct build_a_botApp: App {
+    init() {
+        FirebaseApp.configure()
     }
 
-  }
-
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        // Provide SwiftData context to the whole view tree
+        .modelContainer(for: [Item.self])
+    }
 }
